7604e6c6e10b0bd90b92524e1a201740b739fb6e
Vasiliy <79756790+Lostmindd@users.noreply.github.com>
2024-12-27 20:54:05 +0300

    fall animation created

--- src/main/java/com/main/particlesimulator/Main.java
+++ src/main/java/com/main/particlesimulator/Main.java
@@ -1,6 +1,8 @@
 package com.main.particlesimulator;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -9,25 +11,35 @@ import javafx.scene.text.Text;
 public class Main extends Application{
 
     public static void main(String[] args) {
-
         launch(args);
     }
 
+    Circle circle = new Circle(250.0f, 150.0f, 30.f);
+
+    protected AnimationTimer timer = new AnimationTimer(){
+        @Override
+        public void handle(long now) {
+            if(circle.getCenterY() < 600-30)
+                circle.setCenterY(circle.getCenterY()+10);
+        }
+    };
+
     @Override
     public void start(Stage stage) {
+        // set title for the stage
+        stage.setTitle("creating circle");
 
-        // установка надписи
-        Text text = new Text("text");
-        text.setLayoutY(80);    // установка положения надписи по оси Y
-        text.setLayoutX(80);   // установка положения надписи по оси X
+        // create a Group
+        Group group = new Group(circle);
 
-        Group group = new Group(text);
+        // create a scene
+        Scene scene = new Scene(group, 500, 600);
 
-        Scene scene = new Scene(group);
+        // set the scene
         stage.setScene(scene);
-        stage.setTitle("JavaFX Application");
-        stage.setWidth(300);
-        stage.setHeight(250);
+
         stage.show();
+
+        timer.start();
     }
 }
